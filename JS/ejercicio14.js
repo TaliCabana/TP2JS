13efd2fa3129a9bfc0fe6312a04eb9a994e17dce
Tali <talicabana@gmail.com>
2025-06-22 09:36:46 -0300

    Agregué unas aclaraciones del código del ejercicio 14

--- JS/ejercicio14.js
+++ JS/ejercicio14.js
@@ -1,6 +1,6 @@
 //  14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
 
-const textoUsuario = prompt("Ingresá una frase, y separaré cada caracer con guiones:")
+const textoUsuario = prompt("Ingresá una frase, y separare cada caracer con guiones:")
 
 if (textoUsuario !== null) {
     let textoConGuiones = "" // Aqui agregaré cada caracter con su guión
@@ -8,8 +8,8 @@ if (textoUsuario !== null) {
     for(let i =0 ; i < textoUsuario.length; i++){
         textoConGuiones += textoUsuario.charAt(i)
 
-        if (i<textoUsuario.length - 1) {
-            textoConGuiones += "-";
+        if (i<textoUsuario.length - 1) { // o sea p/c/caracter menos para el último, por eso es "el largo (length) menos 1"
+            textoConGuiones += "-"; // al caracter que no es el último, se le agrega un guión y vuelve a recorrer el bucle
         }
     }
 
